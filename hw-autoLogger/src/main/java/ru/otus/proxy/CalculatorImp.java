4d2caa9a27652ad5beee4aeeb7347c76463134e9
“Sherstobitov” <sherstobitov@finexcons.ru>
2022-05-13 22:09:41 +0500

    Авто-логирование

--- hw-autoLogger/src/main/java/ru/otus/proxy/CalculatorImp.java
+++ hw-autoLogger/src/main/java/ru/otus/proxy/CalculatorImp.java
@@ -5,7 +5,7 @@ public class CalculatorImp implements CalculatorInterface {
     @Log
     @Override
     public void sum(int param1, int param2) {
-        System.out.println("result: " + param1 + param2);
+        System.out.println("result: " + (param1 + param2));
     }
 
     @Override
